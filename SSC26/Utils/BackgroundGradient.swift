abd84e2c211389b05518c7fbbbec197d6e55bbdc
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2026-02-25 19:19:39 +0100

    Change background color in dark mode

--- SSC26/Utils/BackgroundGradient.swift
+++ SSC26/Utils/BackgroundGradient.swift
@@ -6,8 +6,8 @@ struct BackgroundGradient: View {
     var body: some View {
         LinearGradient(
             colors: colorScheme == .dark ? [
-                Color(red: 0.05, green: 0.06, blue: 0.12),
-                Color(red: 0.08, green: 0.06, blue: 0.12)
+                Color(red: 0.08, green: 0.09, blue: 0.15),  // Slightly lighter
+                Color(red: 0.11, green: 0.09, blue: 0.15)   // Slightly lighter
             ] : [
                 Color(red: 0.85, green: 0.93, blue: 1.0),
                 Color(red: 0.96, green: 0.96, blue: 1.0)
